69121b2f664dfcdac0f68b383d527d5fc15f7e00
Ivan Daniluk <ivan.daniluk@gmail.com>
2016-06-02 20:19:49 +0200

    Fix goroutine creation issues

--- convert.go
+++ convert.go
@@ -21,15 +21,17 @@ func ConvertEvents(events []*trace.Event) (Commands, error) {
 			if len(ev.Stk) > 0 {
 				if strings.HasPrefix(ev.Stk[0].Fn, "runtime") {
 					if ev.Stk[0].Fn != "runtime.main" {
+						lastG = ev.Args[0]
 						break
 					}
 				}
 				c.StartGoroutine(ev.Ts, ev.Stk[0].Fn, ev.G, lastG)
 			}
-			lastG = ev.Args[0]
+		case trace.EvGoCreate:
+			lastG = ev.G
 		case trace.EvGoEnd:
 			c.StopGoroutine(ev.Ts, "", ev.G)
-			lastG = 0
+			lastG = 1
 		case trace.EvGoSend:
 			sends.PushBack(ev)
 		case trace.EvGoRecv:
@@ -39,16 +41,15 @@ func ConvertEvents(events []*trace.Event) (Commands, error) {
 				continue
 			}
 			c.ChanSend(send.Ts, ev.Args[1], send.G, ev.G, send.Args[2])
-			//c.ChanRecv(ev.Ts, ev.Args[1], ev.G, ev.Args[0], ev.Args[2])
-		case trace.EvGoBlockSelect:
-			lastG = 0
-		case trace.EvGoStop, trace.EvGoSched, trace.EvGoPreempt,
-			trace.EvGoSleep, trace.EvGoBlock, trace.EvGoBlockSend, trace.EvGoBlockRecv,
+		case trace.EvGCStart, trace.EvGCDone, trace.EvGCScanStart, trace.EvGCScanDone:
+			lastG = 1
+		case trace.EvGoSched, trace.EvGoPreempt,
+			trace.EvGoSleep, trace.EvGoBlock, trace.EvGoBlockSelect, trace.EvGoBlockSend, trace.EvGoBlockRecv,
 			trace.EvGoBlockSync, trace.EvGoBlockCond, trace.EvGoBlockNet,
 			trace.EvGoSysBlock:
 			lastG = 1
-		case trace.EvGoUnblock:
-			lastG = ev.Args[0]
+		case trace.EvGoStop:
+			lastG = 1
 		}
 	}
 
