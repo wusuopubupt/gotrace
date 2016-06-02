5a4f13a5352e2e0ec86cb83fc6bc3216cea2af8c
Ivan Daniluk <ivan.daniluk@gmail.com>
2016-06-02 20:21:57 +0200

    Add caps parameter

--- params.go
+++ params.go
@@ -2,11 +2,13 @@ package main
 
 // Params represents params for GoThree.js library.
 type Params struct {
-	Angle int `json:"angle"`
+	Angle int  `json:"angle"`
+	Caps  bool `json:"allCaps"`
 }
 
 func GuessParams(c Commands) *Params {
 	return &Params{
-		Angle: 5,
+		Angle: 3,
+		Caps:  true,
 	}
 }
