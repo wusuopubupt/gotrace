c8782fdf2822ddbc774b797ea026625d7f2ddfa4
Ivan Daniluk <ivan.daniluk@gmail.com>
2016-06-10 01:10:25 +0200

    Add block example

---
+++ examples/block.go
@@ -0,0 +1,15 @@
+package main
+
+import "time"
+
+func main() {
+	time.Sleep(100 * time.Millisecond)
+	ch := make(chan int)
+	go func() {
+		time.Sleep(100 * time.Millisecond)
+		ch <- 42
+		time.Sleep(100 * time.Millisecond)
+	}()
+	<-ch
+	time.Sleep(100 * time.Millisecond)
+}
