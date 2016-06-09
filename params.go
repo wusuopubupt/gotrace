0d70f5a28b608a17f250bb510b089507beaf6e3b
Ivan Daniluk <ivan.daniluk@gmail.com>
2016-06-09 20:52:24 +0200

    Change params

--- params.go
+++ params.go
@@ -3,7 +3,7 @@ package main
 // Params represents params for GoThree.js library.
 type Params struct {
 	Angle          float64 `json:"angle"`
-	AngleSecond    int     `json:"angle2"`
+	AngleSecond    float64 `json:"angle2"`
 	Caps           bool    `json:"allCaps"`
 	Distance       int     `json:"distance"`
 	DistanceSecond int     `json:"distance2"`
@@ -22,7 +22,7 @@ func GuessParams(cmds Commands) *Params {
 
 	return &Params{
 		Angle:          360.0 / float64(goroutines-1),
-		AngleSecond:    45,
+		AngleSecond:    360.0 / float64(goroutines-1),
 		Caps:           goroutines < 5, // value from head
 		Distance:       80,
 		DistanceSecond: 20,
