e6fd7a97170bbaa66037f3e38ae6cbfe03667672
Ivan Daniluk <ivan.daniluk@gmail.com>
2016-06-10 10:43:30 +0200

    Timeouts increase in example

--- examples/block.go
+++ examples/block.go
@@ -6,7 +6,7 @@ func main() {
 	time.Sleep(100 * time.Millisecond)
 	ch := make(chan int)
 	go func() {
-		time.Sleep(100 * time.Millisecond)
+		time.Sleep(200 * time.Millisecond)
 		ch <- 42
 		time.Sleep(100 * time.Millisecond)
 	}()
