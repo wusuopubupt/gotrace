f657ac645761ddef8a2dd7a5f0e70518544f60f3
Ivan Daniluk <ivan.daniluk@gmail.com>
2016-06-10 14:03:58 +0200

    Add block example

--- examples/block.go
+++ examples/block.go
@@ -1,14 +1,20 @@
 package main
 
+import "math"
 import "time"
 
 func main() {
-	time.Sleep(100 * time.Millisecond)
+	time.Sleep(10 * time.Millisecond)
 	ch := make(chan int)
 	go func() {
-		time.Sleep(200 * time.Millisecond)
+		time.Sleep(10 * time.Millisecond)
+		for i := 0; i < 10000000; i++ {
+			j := math.Sqrt(float64(i * i))
+			j *= j * float64(i)
+			_ = j
+		}
 		ch <- 42
-		time.Sleep(100 * time.Millisecond)
+		time.Sleep(10 * time.Millisecond)
 	}()
 	<-ch
 	time.Sleep(100 * time.Millisecond)
