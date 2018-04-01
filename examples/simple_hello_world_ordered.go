f7dcd335481b0ee832e957c94035050bf2e276c6
Wang Dongxu <wangdongxu@4paradigm.com>
2018-04-01 15:58:07 +0800

    feat: add simple hello world example

---
+++ examples/simple_hello_world_ordered.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"os"
+	"runtime/trace"
+	"time"
+	"fmt"
+)
+
+func main() {
+	trace.Start(os.Stderr)
+
+	i := 0
+	for i = 0; i < 3; i++ {
+		// start new anonymous goroutine
+		go func() {
+			time.Sleep(10 * time.Millisecond)
+			fmt.Println("hello, world from goroutine")
+			time.Sleep(10 * time.Millisecond)
+		}()
+
+		time.Sleep(100 * time.Millisecond)
+	}
+
+	time.Sleep(1 * time.Second)
+
+	trace.Stop()
+}
