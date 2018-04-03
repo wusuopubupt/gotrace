7845c3eacd97cd39a6e42e2b64ef7212331a340a
wusuopubupt <wdxbupt2009@gmail.com>
2018-04-03 23:54:45 +0800

    add time sleep for server1 example

--- examples/server1.go
+++ examples/server1.go
@@ -16,9 +16,9 @@ func handler(c net.Conn) {
 func main() {
 	trace.Start(os.Stderr)
 	fmt.Println("Listening on :5000. Send something using nc: echo hello | nc localhost 5000")
-	fmt.Println("Exiting in 1 second...")
+	fmt.Println("Exiting in 10 second...")
 
-	go time.AfterFunc(1*time.Second, func() { trace.Stop(); os.Exit(0) })
+	go time.AfterFunc(10*time.Second, func() { trace.Stop(); os.Exit(0) })
 
 	l, err := net.Listen("tcp", ":5000")
 	if err != nil {
